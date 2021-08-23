c68d11db8e095882c9f129a4cfdd44519000d2dd
k-yomo <kanji.yy@gmail.com>
2021-08-23 19:00:51 +0900

    Remove k8s and terraform

--- terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  required_version = "0.13.0"
-  backend "gcs" {
-    bucket = "kanji-yomoda-tf-state"
-  }
-}
--- terraform/storage.tf
+++
@@ -1,9 +0,0 @@
-
-resource google_storage_bucket tf_state {
-  project = data.google_project.project.project_id
-  name = "${data.google_project.project.project_id}-tf-state"
-  location = "ASIA"
-  versioning {
-    enabled = true
-  }
-}
